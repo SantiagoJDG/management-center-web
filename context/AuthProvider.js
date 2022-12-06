405df620a3f33eb357774efa1e06adbbe3666058
Edgar A Guevara <eguevara@consultec-ti.com>
2022-12-05 21:31:19 -0500

    add useContext to get session into the app

---
+++ context/AuthProvider.js
@@ -0,0 +1,61 @@
+import { useState, useEffect, createContext } from "react";
+import jwt_decode from 'jwt-decode';
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+
+  const [userToken, setUserToken] = useState();
+  const [waitingUser, setWaitingUser] = useState(true);
+
+  const getUserData = () => {
+    const responsePayload = jwt_decode(userToken)
+    return {
+      ID: responsePayload.sub,
+      name: responsePayload.name,
+      picture: responsePayload.picture,
+      email: responsePayload.email
+    }
+  }
+
+  useEffect(() => {
+
+    const authenticatedUser = async () => {
+
+      const token = sessionStorage.getItem("center-token");
+
+      if (!token) {
+        setWaitingUser(false);
+        return;
+      }
+
+      setUserToken(token);
+
+      setWaitingUser(false);
+    }
+
+    authenticatedUser();
+
+  }, [])
+
+  return (
+    <AuthContext.Provider
+      value={{
+        userToken,
+        setUserToken,
+        getUserData,
+        waitingUser,
+        setWaitingUser
+      }}
+    >
+      {children}
+    </AuthContext.Provider >
+  );
+
+}
+
+export {
+  AuthProvider
+}
+
+export default AuthContext;
