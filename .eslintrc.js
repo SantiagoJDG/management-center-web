a829a8ce81b7a08cabc0d96b21998333e3f68c3c
Edgar A Guevara <eguevara@consultec-ti.com>
2022-12-17 11:32:21 -0500

    chore add step of linting on CI

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,10 +7,10 @@ module.exports = {
     es6: true,
   },
   extends: [
-    'next/core-web-vitals',
     'eslint:recommended',
     'plugin:jsx-a11y/recommended',
     'plugin:prettier/recommended',
+    'next/core-web-vitals',
   ],
   rules: {
     semi: ['error', 'always'],
