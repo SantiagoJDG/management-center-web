52b0d805bd9448c0113718e28573ea4e727a427b
Edgar A Guevara <eguevara@consultec-ti.com>
2023-01-20 21:16:14 -0500

    feat change components into userInfo to adapt those to sidebar

--- src/components/User/UserInfo.js
+++ src/components/User/UserInfo.js
@@ -1,63 +1,59 @@
-import { Box, Chip, Grid, Avatar, Typography } from '@mui/material';
+import { Avatar, Box, Chip, List, ListItem, ListItemText, Typography } from '@mui/material';
 import moment from 'moment';
 import 'moment/locale/es';
 
-const UserInfo = ({ userDataLogged, profilePicture }) => {
+const UserInfo = ({ userDataLogged }) => {
   const admissionDateFormated = moment(userDataLogged.admissionDate).format('LL');
   if (!userDataLogged) {
     return 'There is no user Data';
   } else {
     return (
-      <Grid container direction="column" justifyContent="center" alignItems="center" gap={1}>
-        <Grid item>
-          <Box sx={{ display: 'flex', justifyContent: 'center', padding: 0.5 }}>
-            <Avatar alt={userDataLogged.name} src={profilePicture} />
-          </Box>
-          <Typography variant="h4" align="center">
+      <List dense sx={{ padding: 1.5 }}>
+        <Box sx={{ display: 'flex', justifyContent: 'center', padding: 0.5 }}>
+          <Avatar alt={userDataLogged.name} src={userDataLogged.picture} />
+        </Box>
+        <Box sx={{ mb: 3 }}>
+          <Typography variant="h5" align="center">
+            {' '}
             {userDataLogged.name}
           </Typography>
-        </Grid>
-        <Grid item>{admissionDateFormated}</Grid>
-        <Grid item>{userDataLogged?.supervisorData?.name}</Grid>
-        <Grid item>{userDataLogged?.clientData?.name}</Grid>
-        <Grid item>
-          <Box sx={{ display: 'flex', justifyContent: 'center', padding: 0.5 }}>
-            <Typography variant="h7" align="center">
-              N-1 Perfil:
-            </Typography>
-          </Box>
-          <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
-            {userDataLogged.profiles?.slice(0, 8).map((value, index) => (
-              <Chip key={index} label={value.name} />
-            ))}
-          </Box>
-        </Grid>
-        <Grid item>
-          <Box sx={{ display: 'flex', justifyContent: 'center', padding: 0.5 }}>
-            <Typography variant="h7" align="center">
-              N-2 Conocimientos:
-            </Typography>
-          </Box>
+        </Box>
 
-          <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
-            {userDataLogged.knowledges?.slice(0, 8).map((value, index) => (
+        <ListItem disablePadding>
+          <ListItemText primary="Fecha de ingreso" secondary={admissionDateFormated} />
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemText primary="Supervisor" secondary={userDataLogged.supervisorData.name} />
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemText primary="Cliente" secondary={userDataLogged.clientData.name} />
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemText
+            primary=" N-1 Perfil:"
+            secondary={userDataLogged.profiles.slice(0, 8).map((value, index) => (
               <Chip key={index} label={value.name} />
             ))}
-          </Box>
-        </Grid>
-        <Grid item>
-          <Box sx={{ display: 'flex', justifyContent: 'center', padding: 0.5 }}>
-            <Typography variant="h7" align="center">
-              N-2 Tecnologias:
-            </Typography>
-          </Box>
-          <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
-            {userDataLogged.technologies?.slice(0, 8).map((value, index) => (
+          />
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemText
+            primary=" N-2 Conocimientos:"
+            secondary={userDataLogged.knowledges.slice(0, 8).map((value, index) => (
               <Chip key={index} label={value.name} />
             ))}
-          </Box>
-        </Grid>
-      </Grid>
+          />
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemText
+            primary="  N-3 Tecnologias:"
+            secondary={userDataLogged.technologies?.slice(0, 8).map((value, index) => (
+              <Chip key={index} label={value.name} />
+            ))}
+          />
+        </ListItem>
+      </List>
     );
   }
 };
