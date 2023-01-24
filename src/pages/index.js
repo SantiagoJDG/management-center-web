3dff6b1c37b2f3689090b98468db766573da37d5
Santiago Davila <sdavila@consultec-ti.com>
2023-01-24 18:10:19 -0300

    first layering for planner dashboard

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import GroupsIcon from '@mui/icons-material/Groups';
 import HomeIcon from '@mui/icons-material/Home';
 import PersonIcon from '@mui/icons-material/Person';
+import DashboardIcon from '@mui/icons-material/Dashboard';
 import { List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 import { useRouter } from 'next/router';
 
@@ -25,6 +26,11 @@ export default function Home() {
       path: `/collaborator?id=${userData ? userData.id : ''}`,
       view: ['administrador']
     },
+    {
+      name: 'Planificacion Dashboard',
+      path: '/planner',
+      view: ['administrador']
+    },
     { name: 'Nueva Ficha', path: '/new-collaborator', view: ['administrador'] }
   ];
 
@@ -41,6 +47,13 @@ export default function Home() {
         return (
           <ListItemIcon>
             <GroupsIcon />
+          </ListItemIcon>
+        );
+
+      case '/planner':
+        return (
+          <ListItemIcon>
+            <DashboardIcon />
           </ListItemIcon>
         );
 
