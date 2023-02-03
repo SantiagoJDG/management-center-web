11c728af6b61cdf9f7f5bf4feb9bd2aad1849bba
Edgar A Guevara <eguevara@consultec-ti.com>
2023-02-03 16:43:13 -0500

    feat implementation of @fontsource/roboto

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,8 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import MainLayout from 'layouts/MainLayout';
 import { AuthProvider } from 'context/AuthProvider';
 
