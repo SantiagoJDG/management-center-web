0c03fc640797bd02e09f840d6ca3eb392276e161
Edgar A Guevara <eguevara@consultec-ti.com>
2023-02-09 20:46:01 -0500

    add new snackbar to show message at the app

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,16 +1,18 @@
-import { Box, CssBaseline, Grid } from '@mui/material';
+import { Alert, Box, CssBaseline, Grid, Snackbar } from '@mui/material';
 import Head from 'next/head';
 
 import useAuth from 'hooks/useAuth';
+import useMessage from 'hooks/useMessage';
 
-import { useState } from 'react';
 import Sidebar from 'components/Layout/Sidebar';
+import { useState } from 'react';
 
 const drawerWidth = 240;
 
 const MainLayout = (props) => {
   const { userToken, waitingUser } = useAuth();
   const [mobileOpen, setMobileOpen] = useState(false);
+  const { openMessage, message, handleCloseMessage } = useMessage();
 
   return (
     <>
@@ -25,6 +27,15 @@ const MainLayout = (props) => {
 
       <Grid container>
         <CssBaseline />
+
+        <Snackbar
+          anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+          open={openMessage}
+          Duration={5000}
+          onClose={handleCloseMessage}
+        >
+          <Alert severity={message.severity}>{message.text}</Alert>
+        </Snackbar>
         {!waitingUser && userToken ? (
           <>
             <Sidebar
