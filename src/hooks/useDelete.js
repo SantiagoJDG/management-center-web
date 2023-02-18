5887f461a591f0b9f752725d5bf27b8e3c356179
Edgar A Guevara <eguevara@consultec-ti.com>
2023-02-18 16:21:56 -0500

    feat change at hooks to manage the response as well as we can

--- src/hooks/useDelete.js
+++ src/hooks/useDelete.js
@@ -1,22 +1,24 @@
 import { useState } from 'react';
 import { getAxiosInstance } from 'utils/axiosClient';
 
-const useDelete = (path, body) => {
-  const [isDeleted, setIsDeleted] = useState(false);
+import useMessage from 'hooks/useMessage';
+
+const useDelete = (path) => {
+  const { handleNewMessage } = useMessage();
+
   const deletion = async () => {
     try {
-      await getAxiosInstance()
-        .delete(path, body)
-        .then(() => {
-          setIsDeleted(true);
-        });
+      await getAxiosInstance().delete(path);
     } catch (error) {
-      console.log('error');
-      setIsDeleted(false);
+      console.error('error');
+      handleNewMessage({
+        text: 'Error de comunicación, por favor vuelva a intentar en unos segundos.',
+        severity: 'error'
+      });
     }
   };
 
-  return [deletion, isDeleted];
+  return [deletion];
 };
 
 export default useDelete;
