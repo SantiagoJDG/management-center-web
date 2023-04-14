463203c4402525a9074795f196ee2bc3b0045a0f
Santiago Davila <sdavila@consultec-ti.com>
2023-04-14 14:56:17 -0300

    CollaboratorBarFilter tests complete

--- src/__test__/CollaboratorBarFilter.test.js
+++ src/__test__/CollaboratorBarFilter.test.js
@@ -1,48 +1,88 @@
-import { fireEvent, render, screen, within } from '@testing-library/react';
+import { render, fireEvent, screen, within } from '@testing-library/react';
 import React from 'react';
+
+import { act } from 'react-dom/test-utils';
+import { getAxiosInstance } from 'utils/axiosClient';
+
 import CollaboratorBarFilter from '../components/Collaborators/CollaboratorBarFilter';
 import useMessage from 'hooks/useMessage';
 import '@testing-library/jest-dom';
 
-jest.mock('axios');
+jest.mock('utils/axiosClient');
+
 jest.mock('hooks/useMessage', () => ({
   __esModule: true,
   default: jest.fn()
 }));
 
-describe('CollaboratorBarFilter component', () => {
-  let setCollaborators;
-  let useEffect;
-
-  const mockUseEffect = () => {
-    useEffect.mockImplementation(() => {});
-  };
-
+describe('CollaboratorBarFilter', () => {
   beforeEach(() => {
-    setCollaborators = jest.fn();
-    useEffect = jest.spyOn(React, 'useEffect');
-    mockUseEffect();
-  });
-
-  afterEach(() => {
     jest.clearAllMocks();
   });
 
-  it('Must render correctly', () => {
+  const mockData = [
+    { id: 1, name: 'Venezuela' },
+    { id: 2, name: 'Argentina' }
+  ];
+
+  it('Should fetch filter data', async () => {
     useMessage.mockReturnValue({ handleNewMessage: jest.fn() });
-    const { container } = render(<CollaboratorBarFilter setCollaborators={setCollaborators} />);
-    expect(container).toBeInTheDocument();
+
+    getAxiosInstance.mockImplementation(() => ({
+      get: jest.fn(() => Promise.resolve({ data: mockData }))
+    }));
+
+    await act(async () => {
+      render(<CollaboratorBarFilter setCollaborators={() => {}} />);
+    });
+
+    expect(getAxiosInstance).toHaveBeenCalledTimes(8);
   });
 
-  test('executeFilter is called when an item is selected from a dropdown', () => {
-    const executeFilter = jest.fn();
-    const { getAllByRole, getByRole } = render(
-      <CollaboratorBarFilter setCollaborators={setCollaborators} />
-    );
-    const listbox = getAllByRole('button');
-    fireEvent.mouseDown(listbox[0]);
-    const listbox2 = within(getByRole('listbox'));
-    fireEvent.click(listbox2);
-    expect(executeFilter).toHaveBeenCalled();
+  it('Should handle error when fetching filter data', async () => {
+    const consoleSpy = jest.spyOn(console, 'error');
+    getAxiosInstance.mockImplementation(() => ({
+      get: jest.fn(() => Promise.reject({}))
+    }));
+
+    await act(async () => {
+      render(<CollaboratorBarFilter setCollaborators={() => {}} />);
+    });
+
+    expect(consoleSpy).toBeTruthy();
+  });
+
+  it('Should fill the Dropdown list information', async () => {
+    useMessage.mockReturnValue({ handleNewMessage: jest.fn() });
+
+    getAxiosInstance.mockImplementation(() => ({
+      get: jest.fn(() => Promise.resolve({ data: mockData }))
+    }));
+
+    await act(async () => {
+      render(<CollaboratorBarFilter setCollaborators={() => {}} />);
+    });
+
+    fireEvent.mouseDown(screen.getAllByRole('button')[0]);
+    const listbox = within(screen.getByRole('listbox'));
+    expect(listbox.getByText(/Argentina/i)).toBeTruthy();
+  });
+
+  it('Should execute getAxiosInstance on click an Element in the dropdown', async () => {
+    useMessage.mockReturnValue({ handleNewMessage: jest.fn() });
+    const mockSetCollaborators = jest.fn();
+    getAxiosInstance.mockImplementation(() => ({
+      get: jest.fn(() => Promise.resolve({ data: mockData }))
+    }));
+
+    await act(async () => {
+      render(<CollaboratorBarFilter setCollaborators={mockSetCollaborators} />);
+    });
+
+    fireEvent.mouseDown(screen.getAllByRole('button')[0]);
+    const listbox = within(screen.getByRole('listbox'));
+    fireEvent.click(listbox.getByText(/Argentina/i));
+
+    expect(getAxiosInstance).toHaveBeenCalledTimes(9);
   });
 });
